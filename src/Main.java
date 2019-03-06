dcd4f2e953d6c4f6668d86e27efe5f362aa2d85f
Alvaro Lira Palomino <alvarolirap@client185-10-192-212.eduroam.upc.edu>
2019-03-06 17:07:58 +0100

    Enlace librerías

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import IA.Comparticion.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Usuario us = new Usuario(1,2,3,4,true);
+        System.out.println(us.getCoordDestinoX());
+    }
+}
