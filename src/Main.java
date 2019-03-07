b7f8f6f63a67f246b3bad5e91b3948aaec6232f5
Alvaro Lira Palomino <alvarolirap@MacBook-Pro-de-Alvaro.local>
2019-03-07 02:15:46 +0100

    State v. 1.1

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
         Usuario us = new Usuario(1,2,3,4,true);
-        System.out.println(us.getCoordDestinoX());
-        State s = new State(1,2);
-        System.out.println(s.toString());
+        Usuarios usuarios = new Usuarios(5, 3,1);
+        State s1 = new State(1,2);
+        State s2 = new State(usuarios);
+        System.out.println(s1.toString());
+        System.out.println(s2.toString());
+        System.out.println(s2.getRoute(2));
     }
 }
--- src/State.java
+++ src/State.java
@@ -1,3 +1,8 @@
+import IA.Comparticion.Usuario;
+import IA.Comparticion.Usuarios;
+
+import java.util.Iterator;
+
 public class State {
 
     private int [][] routes;
@@ -7,14 +12,39 @@ public class State {
     public State(int n, int m) {
         //n: numero de personas apuntadas
         //m: numero de personas que pueden conducir
-        this.nApuntados = n;
-        this.mConducir = m;
-        this.routes = new int[m][n];
+        nApuntados = n;
+        mConducir = m;
+        routes = new int[m][2*n+1];
+    }
+    public State(Usuarios usuarios) {
+        Usuario tmp;
+        mConducir = 0;
+        nApuntados = usuarios.size();
+        int[] tmpDrivers = new int[nApuntados];
+        Iterator<Usuario> iterator = usuarios.iterator();
+        while (iterator.hasNext()) {
+            tmp = iterator.next();
+            if(tmp.isConductor()){
+                tmpDrivers[mConducir] = tmp.hashCode();
+                mConducir ++;
+            }
+        }
+        routes = new int[mConducir][2*nApuntados + 1];
+        for(int i=0;i<mConducir;i++){
+            routes[i][0] = tmpDrivers[i];
+        }
+    }
+    public String getRoute(int n){
+        String retVal = "";
+        for(int j=0;j<=2*nApuntados;j++){
+            retVal += routes[n-1][j] + " ";
+        }
+        return retVal;
     }
     public String toString() {
         String retVal = "";
         for(int i=0;i<mConducir;i++){
-            for(int j=0;j<nApuntados;j++){
+            for(int j=0;j<=2*nApuntados;j++){
                 retVal += routes[i][j] + " ";
             }
             retVal += "\n";
