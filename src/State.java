869fb4340724424cf46b323e1c793a8224f082ad
DJA <drod89@gmail.com>
2019-03-10 19:27:47 +0100

    Cleaning up code style

--- src/State.java
+++ src/State.java
@@ -12,6 +12,7 @@ public class State {
     private int mConducir;
     private String ERROR_DRIVER = "ERROR: CONDUCTOR NO ENCONTRADO";
     private String ERROR_PASSENGER = "ERROR: PASAJERO NO ENCONTRADO";
+
     public State(int n, int m) {
         //n: numero de personas apuntadas
         //m: numero de personas que pueden conducir
@@ -19,10 +20,11 @@ public class State {
         mConducir = m;
         drivers = new int[m];
         routes = new ArrayList[mConducir];
-        for(int i=0;i<mConducir;i++){
+        for(int i = 0; i < mConducir; i++) {
             routes[i] = new ArrayList<Integer>();
         }
     }
+
     public State(Usuarios usuarios) {
         Usuario tmp;
         mConducir = 0;
@@ -31,58 +33,62 @@ public class State {
         Iterator<Usuario> iterator = usuarios.iterator();
         while (iterator.hasNext()) {
             tmp = iterator.next();
-            if(tmp.isConductor()){
+            if(tmp.isConductor()) {
                 tmpDrivers[mConducir] = tmp.hashCode();
                 mConducir ++;
             }
         }
         drivers = new int[mConducir];
         routes = new ArrayList[mConducir];
-        for(int i=0;i<mConducir;i++){
+        for(int i = 0; i < mConducir; i++) {
             routes[i] = new ArrayList<Integer>();
             drivers[i] = tmpDrivers[i];
         }
     }
-    public void pass(Usuario driver, Usuario passenger){
+
+    public void pass(Usuario driver, Usuario passenger) {
         int pos = -1;
-        for(int i=0;i<mConducir;i++){
-            if(drivers[i] == driver.hashCode()){
+        for(int i = 0; i < mConducir; i++) {
+            if(drivers[i] == driver.hashCode()) {
                 pos = i;
             }
         }
-        if(pos != -1){
+        if(pos != -1) {
             routes[pos].add(passenger.hashCode());
         }
     }
-    public String getRoute(int n){
+
+    public String getRoute(int n) {
         String retVal = "id: " + drivers[n-1]+ ", route size: " + routes[n-1].size();
-        for(int j=0;j<routes[n-1].size();j++){
+        for(int j = 0; j < routes[n-1].size(); j++) {
             retVal += " " + routes[n-1].get(j);
         }
         return retVal;
     }
-    public String getRoute(Usuario driver){
+
+    public String getRoute(Usuario driver) {
         int pos = -1;
-        for(int i=0;i<mConducir;i++){
-            if(drivers[i] == driver.hashCode()){
+        for(int i = 0; i < mConducir; i++) {
+            if(drivers[i] == driver.hashCode()) {
                 pos = i;
             }
         }
         if(pos != -1){
             String retVal = "id: " + drivers[pos]+ ", route size: " + routes[pos].size();
-            for(int j=0;j<routes[pos].size();j++){
+            for(int j = 0; j < routes[pos].size(); j++) {
                 retVal += " " + routes[pos].get(j);
             }
             return retVal;
-        }else{
+        } else {
             return ERROR_DRIVER;
         }
     }
+
     public String toString() {
         String retVal = "";
-        for(int i=0;i<mConducir;i++){
+        for(int i = 0; i < mConducir; i++) {
             retVal += "id: " + drivers[i]+ ", route size: " + routes[i].size();
-            for(int j=0;j<routes[i].size();j++){
+            for(int j = 0; j < routes[i].size(); j++) {
                 retVal += " " + routes[i].get(j);
             }
             retVal += "\n";
