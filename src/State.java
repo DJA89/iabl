eb62a2fe92b596422278b38bd65e467aaac06b3b
Joseba <joseba.sierra@est.fib.upc.edu>
2019-03-17 21:05:22 +0100

    Update distancias en algunas funciones + cambios y correcciones

--- src/State.java
+++ src/State.java
@@ -15,39 +15,90 @@ import IA.Comparticion.Usuarios;
 public class State {
 
     private ArrayList[] routes;
+    private  int[] routesDistance;
 
-    private static int[] routesDistance;
+    private static int N;
+    private static int[][] distances;  // distances[i,N+j] = d(punto recogida pasajero i - punto dejada j)
+    private static Position[][] usersInfo;
+
     private static int[] drivers;
     private static int[] passengers;
     private static int[] users;
     private static Usuarios usersList;
-    private static int[][] distances;  // distances[i,2*j] = d(punto recogida pasajero i - punto dejada j)
-    private static Position[][] usersInfo;
 
 
-    private void ComputeAllDistances() {
-        int n = usersInfo.length;
-        distances = new int[2*n][2*n];
-        for(int i = 0; i < n; i++) {
+    public class InfoParada {
+        public int passenger;
+        public boolean dejada;
+    }
 
-            Position pOrigen_i = usersInfo[i][0];
-            Position pDestino_i = usersInfo[i][1];
 
-            for(int j = i; j < n; j++) {
-                Position pOrigen_j = usersInfo[j][0];
-                Position pDestino_j = usersInfo[j][1];
 
-                distances[i][j] = distances[j][i] = ComputeDistance(pOrigen_i, pOrigen_j);
-                distances[2*i][j] = distances[j][i] = ComputeDistance(pDestino_i, pOrigen_j);
-                distances[i][2*j] = distances[j][i] = ComputeDistance(pOrigen_i,pDestino_j);
-                distances[2*i][2*j] = distances[j][i] = ComputeDistance(pDestino_i, pDestino_j);
-           }
+    public State(int mDrivers, Usuarios usuarios) {
+
+        //m: numero de personas que pueden conducir
+        routes = new ArrayList[mDrivers];
+        for(int i = 0; i < mDrivers; i++) {
+            routes[i] = new ArrayList<InfoParada>();
+
         }
+
+        N = usuarios.size();
+        usersInfo = new Position[N][2];
+        int i = 0;
+        for (Usuario user: usuarios) {
+            Position coordOrigen = new Position(user.getCoordOrigenX(), user.getCoordOrigenY());
+            Position coordDestino = new Position(user.getCoordDestinoX(), user.getCoordDestinoY());
+            usersInfo[i][0] = coordOrigen;
+            usersInfo[i][1] = coordDestino;
+            i++;
+        }
+
+        ComputeAllDistances();
     }
 
     public State(Usuarios userList){
         setUserList(userList);
     }
+
+    private void ComputeAllDistances() {
+
+        distances = new int[2*N][2*N];
+        for(int i = 0; i < N; i++) {
+
+            Position pOrigen_i = usersInfo[i][0];
+            Position pDestino_i = usersInfo[i][1];
+
+            for(int j = i; j < N; j++) {
+                Position pOrigen_j = usersInfo[j][0];
+                Position pDestino_j = usersInfo[j][1];
+
+                distances[i][j] = distances[j][i] = ComputeDistance(pOrigen_i, pOrigen_j);
+                distances[N+i][j] = distances[j][N+i] = ComputeDistance(pDestino_i, pOrigen_j);
+                distances[i][N+j] = distances[N+j][i] = ComputeDistance(pOrigen_i,pDestino_j);
+                distances[N+i][N+j] = distances[N+j][N+i] = ComputeDistance(pDestino_i, pDestino_j);
+           }
+        }
+    }
+
+    private int ComputeDistance(Position p1, Position p2) {
+        // d(i,j) = /ix - jx/ + /iy - jy/
+        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+    }
+
+    private int GetDistance(InfoParada p1, InfoParada p2) {
+
+        int x1 = p1.passenger;
+        if (p1.dejada) x1 = x1 + N;
+
+        int x2 = p2.passenger;
+        if (p2.dejada) x2 = x2 + N;
+
+        return distances[x1][x2];
+    }
+
+
+
 
     public void setUserList(Usuarios usersList) {
         Usuario tmp;
@@ -109,37 +160,7 @@ public class State {
         }
     }
 
-    private int ComputeDistance(Position p1, Position p2) {
-        // d(i,j) = /ix - jx/ + /iy - jy/
-        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
-    }
 
-
-
-    public State(int mDrivers, Usuarios usuarios) {
-
-        //m: numero de personas que pueden conducir
-        routes = new ArrayList[mDrivers];
-        for(int i = 0; i < mDrivers; i++) {
-            routes[i] = new ArrayList<Integer>();
-
-        }
-
-
-
-        int n = usuarios.size();
-        usersInfo = new Position[n][2];
-        int i = 0;
-        for (Usuario user: usuarios) {
-            Position coordOrigen = new Position(user.getCoordOrigenX(), user.getCoordOrigenY());
-            Position coordDestino = new Position(user.getCoordDestinoX(), user.getCoordDestinoY());
-            usersInfo[i][0] = coordOrigen;
-            usersInfo[i][1] = coordDestino;
-            i++;
-        }
-
-        ComputeAllDistances();
-    }
 
     public void pass(int driver_pos, int passenger_id) {
         routes[driver_pos].add(passenger_id);
@@ -169,15 +190,36 @@ public class State {
     public void SwapRouteOrder(int c, int pos1, int pos2) {}
 
     public void MovePassenger(int cOrigen, int cDestino, int passenger, int posRecogida, int posDejada) {
-        RemovePassenger(routes[cOrigen], passenger);
+
+        RemovePassenger(routes[cOrigen], cOrigen, passenger);
         PutPassenger(routes[cDestino], passenger, posRecogida, posDejada);
     }
 
-    private void RemovePassenger(ArrayList<Integer> ruta, int passenger) {
-        ruta.remove(passenger); //removes first occurrence of the passenger
-        ruta.remove(passenger); //removes second occurrence of the passenger
+    //elimina el punto de recogida y de dejada de un pasajero y actualiza la distancia de la ruta del conductor resultante
+    private void RemovePassenger(ArrayList<InfoParada> ruta, int conductor, int passenger) {
+
+        int i = ruta.indexOf(passenger);
+        UpdateDistanceRemoved(ruta, conductor,i-1,i,i+1);
+        ruta.remove(i);
+
+        i = ruta.indexOf(passenger);
+        UpdateDistanceRemoved(ruta, conductor,i-1,i,i+1);
+        ruta.remove(i);
     }
 
+    //actualiza la distancia de la ruta al eliminar la parada numero j con parada adjacente i-k
+    private void UpdateDistanceRemoved(ArrayList<InfoParada> ruta, int conductor,int i,int j,int k) {
+
+
+        InfoParada a = ruta.get(i);
+        InfoParada b = ruta.get(i);
+        InfoParada c = ruta.get(i);
+
+        routesDistance[conductor] = routesDistance[conductor] -  GetDistance(a,b) - GetDistance(b,c) +  GetDistance(a,b);
+
+    }
+
+    //TODO: falta hacer como con remove, el update de la distancia de la ruta etc..
     private void PutPassenger(ArrayList<Integer> ruta, int passenger, int pos1, int pos2) {
         ruta.add(pos1, passenger);
         ruta.add(pos2, passenger);
