4f2e3e46fd815b6822aecb7c9e4da74151b996fe
Alvaro Lira Palomino <alvarolirap@MacBook-Pro-de-Alvaro.local>
2019-03-18 20:19:57 +0100

    Ordenar

--- src/State.java
+++ src/State.java
@@ -1,22 +1,11 @@
 import IA.Comparticion.Usuario;
 import IA.Comparticion.Usuarios;
-
 import java.util.ArrayList;
-
 import java.util.Arrays;
-import java.util.Iterator;
-import IA.Comparticion.Usuario;
-import IA.Comparticion.Usuarios;
-
-
-
-
 
 public class State {
 
     private ArrayList[] routes;
-
-    private static int[] routesDistance;
     private static int[] drivers;
     private static int[] passengers;
     private static int[] users;
@@ -24,60 +13,59 @@ public class State {
     private static int[][] distances;  // distances[i,2*j] = d(punto recogida pasajero i - punto dejada j)
     private static Position[][] usersInfo;
 
-
-    private void ComputeAllDistances() {
-        int n = usersInfo.length;
-        distances = new int[2*n][2*n];
-        for(int i = 0; i < n; i++) {
-
-            Position pOrigen_i = usersInfo[i][0];
-            Position pDestino_i = usersInfo[i][1];
-
-            for(int j = i; j < n; j++) {
-                Position pOrigen_j = usersInfo[j][0];
-                Position pDestino_j = usersInfo[j][1];
-
-                distances[i][j] = distances[j][i] = ComputeDistance(pOrigen_i, pOrigen_j);
-                distances[2*i][j] = distances[j][i] = ComputeDistance(pDestino_i, pOrigen_j);
-                distances[i][2*j] = distances[j][i] = ComputeDistance(pOrigen_i,pDestino_j);
-                distances[2*i][2*j] = distances[j][i] = ComputeDistance(pDestino_i, pDestino_j);
-           }
-        }
-    }
-
     public State(Usuarios userList){
         setUserList(userList);
     }
 
     public void setUserList(Usuarios usersList) {
-        Usuario tmp;
         ArrayList<Integer> tmpDrivers = new ArrayList<Integer>();
         ArrayList<Integer> tmpPassengers = new ArrayList<Integer>();
         ArrayList<Integer> tmpUsers = new ArrayList<Integer>();
-        Iterator<Usuario> iterator = usersList.iterator();
-        while (iterator.hasNext()) {
-            tmp = iterator.next();
-            if(tmp.isConductor()) {
-                tmpDrivers.add(tmp.hashCode());
+        usersInfo = new Position[usersList.size()][2];
+        int i = 0;
+        for (Usuario user: usersList) {
+            usersInfo[i][0] = new Position(user.getCoordOrigenX(), user.getCoordOrigenY()); //Origen
+            usersInfo[i][1] = new Position(user.getCoordDestinoX(), user.getCoordDestinoY()); //Destino;
+            i++;
+            if(user.isConductor()) {
+                tmpDrivers.add(user.hashCode());
             }else{
-                tmpPassengers.add(tmp.hashCode());
+                tmpPassengers.add(user.hashCode());
             }
-            tmpUsers.add(tmp.hashCode());
+            tmpUsers.add(user.hashCode());
         }
         drivers = Arrays.stream(tmpDrivers.toArray(new Integer[tmpDrivers.size()])).mapToInt(Integer::intValue).toArray();
         passengers = Arrays.stream(tmpPassengers.toArray(new Integer[tmpPassengers.size()])).mapToInt(Integer::intValue).toArray();
         users = Arrays.stream(tmpUsers.toArray(new Integer[tmpUsers.size()])).mapToInt(Integer::intValue).toArray();
         this.usersList = usersList;
         routes = new ArrayList[drivers.length];
-        clearRoutes();
+        initRoutes();
+        ComputeAllDistances();
     }
 
-    private void clearRoutes(){
+    private void initRoutes(){
         for(int i = 0; i < drivers.length; i++) routes[i] = new ArrayList<Integer>();
     }
 
+    private void ComputeAllDistances() {
+        int n = usersInfo.length;
+        distances = new int[2*n][2*n];
+        for(int i = 0; i < n; i++) {
+            Position pOrigen_i = usersInfo[i][0];
+            Position pDestino_i = usersInfo[i][1];
+            for(int j = i; j < n; j++) {
+                Position pOrigen_j = usersInfo[j][0];
+                Position pDestino_j = usersInfo[j][1];
+                distances[i][j] = distances[j][i] = ComputeDistance(pOrigen_i, pOrigen_j);
+                distances[2*i][j] = distances[j][i] = ComputeDistance(pDestino_i, pOrigen_j);
+                distances[i][2*j] = distances[j][i] = ComputeDistance(pOrigen_i,pDestino_j);
+                distances[2*i][2*j] = distances[j][i] = ComputeDistance(pDestino_i, pDestino_j);
+            }
+        }
+    }
+
     public void donkeyInit(){
-        clearRoutes();
+        initRoutes();
         pass(0, drivers[0]);
         for(int i=0;i<users.length;i++){
             if(users[i] != drivers[0]){
@@ -89,7 +77,7 @@ public class State {
     }
 
     public void averageInit() {
-        clearRoutes();
+        initRoutes();
         int average = passengers.length / drivers.length;
         int passenger_pos = 0, j;
         for (int i = 0; i < drivers.length; i++) {
@@ -109,57 +97,17 @@ public class State {
         }
     }
 
+    public void minRouteInit() {
+        initRoutes();
+    }
+
     private int ComputeDistance(Position p1, Position p2) {
         // d(i,j) = /ix - jx/ + /iy - jy/
         return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
     }
 
-
-
-    public State(int mDrivers, Usuarios usuarios) {
-
-        //m: numero de personas que pueden conducir
-        routes = new ArrayList[mDrivers];
-        for(int i = 0; i < mDrivers; i++) {
-            routes[i] = new ArrayList<Integer>();
-
-        }
-
-
-
-        int n = usuarios.size();
-        usersInfo = new Position[n][2];
-        int i = 0;
-        for (Usuario user: usuarios) {
-            Position coordOrigen = new Position(user.getCoordOrigenX(), user.getCoordOrigenY());
-            Position coordDestino = new Position(user.getCoordDestinoX(), user.getCoordDestinoY());
-            usersInfo[i][0] = coordOrigen;
-            usersInfo[i][1] = coordDestino;
-            i++;
-        }
-
-        ComputeAllDistances();
-    }
-
     public void pass(int driver_pos, int passenger_id) {
         routes[driver_pos].add(passenger_id);
-    }
-
-    public String getRoute(int driver_pos) {
-        String retVal = "Size: " + routes[driver_pos-1].size() + ": ";
-        for(int i = 0; i < routes[driver_pos-1].size(); i++) {
-            retVal += " " + routes[driver_pos-1].get(i);
-        }
-        retVal += "\n";
-        return retVal;
-    }
-
-    public String toString() {
-        String retVal = "";
-        for(int i = 0; i < routes.length; i++) {
-            retVal += getRoute(i + 1);
-        }
-        return retVal;
     }
 
     public void SwapPassenger(int c1, int c2, int p1, int p2, int posip1, int posjp1, int posip2, int posjp2 ) {}
@@ -181,6 +129,23 @@ public class State {
     private void PutPassenger(ArrayList<Integer> ruta, int passenger, int pos1, int pos2) {
         ruta.add(pos1, passenger);
         ruta.add(pos2, passenger);
+    }
+
+    public String getRoute(int driver_pos) {
+        String retVal = "Size: " + routes[driver_pos-1].size() + ": ";
+        for(int i = 0; i < routes[driver_pos-1].size(); i++) {
+            retVal += " " + routes[driver_pos-1].get(i);
+        }
+        retVal += "\n";
+        return retVal;
+    }
+
+    public String toString() {
+        String retVal = "";
+        for(int i = 0; i < routes.length; i++) {
+            retVal += getRoute(i + 1);
+        }
+        return retVal;
     }
 
 }
