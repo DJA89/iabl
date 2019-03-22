f66acda8f02c3fecf80af98494e22d1f7b429a70
Alvaro Lira Palomino <alvarolirap@MacBook-Pro-de-Alvaro.local>
2019-03-22 05:40:08 +0100

    MinInit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ import IA.Comparticion.*;
 public class Main {
 
     public static void main(String[] args) {
-        State s = new State(new Usuarios(10, 4,1));
+        State s = new State(new Usuarios(10, 4, 1));
+        s.ImprimirDistancias();
+        s.ImprimirPosiciones();
         s.donkeyInit();
-        System.out.println("Donkey Init\n" + s.toString());
+        System.out.println("Donkey Init");
+        s.ImprimirConductores();
         s.averageInit();
-        System.out.println("Average Init\n" + s.toString());
+        System.out.println("Average Init");
+        s.ImprimirConductores();
+        s.minRouteInit();
+        System.out.println("Min Route Init");
+        s.ImprimirConductores();
     }
 }
